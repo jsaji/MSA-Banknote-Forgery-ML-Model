b4b275d56da2f65f82ddd4cb29cb1c80496e44fe
justinsaji <justinsaji1@gmail.com>
2020-06-22 23:11:40 +1000

    Splits data into train & test sets

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ Used for creating, running and utilising the ForgeryDetector ML object
 from model import ForgeryDetector
 
 model = ForgeryDetector()
-model.plot_all()
+#model.plot_all()
+model.train()
--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class ForgeryDetector():
         data_set = data_set.round(2)
         self.data_set = data_set
 
-    def plot_features(self, feature_1, feature_2):
+    def plot_two_features(self, feature_1, feature_2):
         '''Plots any two features of the data set using their column name'''
         scatter = graph.scatter(self.data_set[feature_1], self.data_set[feature_2], c=self.data_set['is_forged'], marker='D')
         graph.xlabel(feature_1)
@@ -34,7 +34,6 @@ class ForgeryDetector():
         fig = graph.figure()
         ax = fig.add_subplot(111, projection='3d')
         scatter = ax.scatter(self.data_set[feature_1], self.data_set[feature_2], self.data_set[feature_3], c=self.data_set['is_forged'])
-        print(self.data_set.head())
         ax.set_xlabel(feature_1)
         ax.set_ylabel(feature_2)
         ax.set_zlabel(feature_3)
@@ -48,7 +47,7 @@ class ForgeryDetector():
         '''
         for i in range(len(self.data_set.columns)):
             for j in range(i+1, len(self.data_set.columns), 1):
-                self.plot_features(self.data_set.columns[i], self.data_set.columns[j])
+                self.plot_two_features(self.data_set.columns[i], self.data_set.columns[j])
         '''
         # Plotting each pair does not provide a conclusive set of features to train with
         '''Plots every triplet of features'''
@@ -59,14 +58,32 @@ class ForgeryDetector():
                     self.plot_three_features(self.data_set.columns[i], self.data_set.columns[j],self.data_set.columns[k])
         # The most promising combination is variance, curtosis and skewness (this plot had distinctive separation between forged/not forged)
                 
-
-
-    def something(self):
-        '''Don't know what to do here yet'''
+    def train(self):
+        '''
         all_features = self.data_set.drop(['is_forged'], axis=1)
         variance_feat = self.data_set['variance'].values
         curtosis_feat = self.data_set['curtosis'].values
         skewness_feat = self.data_set['skewness'].values
         entropy_feat = self.data_set['entropy'].values
         is_forged = self.data_set['is_forged'].values
+        '''
+        
+        train_data = self.data_set.sample(frac=0.8)
+        test_data = self.data_set.drop(train_data.index).reset_index(drop=True)
+        train_data = train_data.reset_index(drop=True)
+
+        train_X = train_data.drop(['entropy', 'is_forged'], 1)
+        train_Y = train_data['is_forged']
+
+        test_X = test_data.drop(['entropy', 'is_forged'], 1)
+        test_Y = test_data['is_forged']
+
+        '''
+        print(train_X.describe())
+        print(train_Y.describe())
+
+        print(test_X.describe())
+        print(test_Y.describe())
+        '''
+
 
