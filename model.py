d08f210cb9aaf3a5f21c2046d5c554ccb3a0af43
justinsaji <justinsaji1@gmail.com>
2020-06-22 22:25:58 +1000

    Added 3D scatter plot to determine best feature triplet

--- model.py
+++ model.py
@@ -30,11 +30,36 @@ class ForgeryDetector():
         graph.grid()
         graph.show()
 
+    def plot_three_features(self, feature_1, feature_2, feature_3):
+        fig = graph.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        scatter = ax.scatter(self.data_set[feature_1], self.data_set[feature_2], self.data_set[feature_3], c=self.data_set['is_forged'])
+        print(self.data_set.head())
+        ax.set_xlabel(feature_1)
+        ax.set_ylabel(feature_2)
+        ax.set_zlabel(feature_3)
+        title = 'Classification Plot using ' + feature_1 + ', ' + feature_2 + ' & ' + feature_3
+        graph.legend(handles=scatter.legend_elements()[0], labels=["not forged", "forged"])
+        graph.title(title)
+        graph.show()
+
     def plot_all(self):
         '''Plots every pair of features'''
+        '''
         for i in range(len(self.data_set.columns)):
             for j in range(i+1, len(self.data_set.columns), 1):
                 self.plot_features(self.data_set.columns[i], self.data_set.columns[j])
+        '''
+        # Plotting each pair does not provide a conclusive set of features to train with
+        '''Plots every triplet of features'''
+        total = len(self.data_set.columns) - 1
+        for i in range(total):
+            for j in range(i + 1, total, 1):
+                for k in range(j + 1,total, 1):
+                    self.plot_three_features(self.data_set.columns[i], self.data_set.columns[j],self.data_set.columns[k])
+        # The most promising combination is variance, curtosis and skewness (this plot had distinctive separation between forged/not forged)
+                
+
 
     def something(self):
         '''Don't know what to do here yet'''
