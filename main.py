ee366b9e891099360b5235d1fab510561dcea84a
justinsaji <justinsaji1@gmail.com>
2020-06-24 16:18:11 +1000

    Improved display of test classification + predict function & extra error handling

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 main.py
 Used for creating, running and utilising the ForgeryDetector ML object
 '''
+import pandas as pd
 from model import ForgeryDetector
 
-model = ForgeryDetector()
+model = ForgeryDetector('Resources/banknote_data.txt', 0.8)
 #model.plot_all()
-model.train()
+model.train_and_test()
+test_data = pd.DataFrame([[3.6216,8.6661,-2.8073],[-2.8391,-6.63,10.4849]], columns=['variance', 'curtosis', 'skewness'])
+pred = (model.predict(test_data))
+print(str(pred))
--- model.py
+++ model.py
@@ -6,27 +6,32 @@ Defines models used in the project
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as graph
-from sklearn import svm
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split
 
 
 class ForgeryDetector():
     '''
     Defines an ML model to analyze data from scanned banknotes to determine if they're legitimate or forged
     '''
-    def __init__(self):
-        data_set = pd.read_csv('Resources/banknote_data.txt', sep=',', header=None)
-        data_set.columns = ['variance', 'curtosis', 'skewness', 'entropy', 'is_forged']
-        data_set = data_set.round(2)
-        self.data_set = data_set
+    def __init__(self, filename, train_split=0.8):
+        try:
+            data_set = pd.read_csv(filename, sep=',', header=None)
+            data_set.columns = ['variance', 'curtosis', 'skewness', 'entropy', 'is_forged']
+            self.data_set = data_set
+            self.svm_model = None
+            self.train_split = train_split
+        except:
+            print('Ensure a valid data file is used')
+            raise
 
     def plot_two_features(self, feature_1, feature_2):
         '''Plots any two features of the data set using their column name'''
         scatter = graph.scatter(self.data_set[feature_1], self.data_set[feature_2], c=self.data_set['is_forged'], marker='D')
         graph.xlabel(feature_1)
         graph.ylabel(feature_2)
-        title = 'Classification Plot using ' + feature_1 + ' & ' + feature_2
-        graph.title(title)
-        graph.legend(handles=scatter.legend_elements()[0], labels=["not forged", "forged"])
+        graph.title('Classification Plot using ' + feature_1 + ' & ' + feature_2)
+        graph.legend(handles=scatter.legend_elements()[0], labels=['not forged', 'forged'])
         graph.grid()
         graph.show()
 
@@ -37,9 +42,8 @@ class ForgeryDetector():
         ax.set_xlabel(feature_1)
         ax.set_ylabel(feature_2)
         ax.set_zlabel(feature_3)
-        title = 'Classification Plot using ' + feature_1 + ', ' + feature_2 + ' & ' + feature_3
-        graph.legend(handles=scatter.legend_elements()[0], labels=["not forged", "forged"])
-        graph.title(title)
+        graph.legend(handles=scatter.legend_elements()[0], labels=['not forged', 'forged'])
+        graph.title('Classification Plot using ' + feature_1 + ', ' + feature_2 + ' & ' + feature_3 + ' (all data)')
         graph.show()
 
     def plot_all(self):
@@ -56,55 +60,43 @@ class ForgeryDetector():
             for j in range(i + 1, total, 1):
                 for k in range(j + 1,total, 1):
                     self.plot_three_features(self.data_set.columns[i], self.data_set.columns[j],self.data_set.columns[k])
-        # The most promising combination is variance, curtosis and skewness (this plot had distinctive separation between forged/not forged)
+        # The most promising combination is variance, curtosis and skewness (this plot has distinctive separation between forged/not forged)
                 
-    def train(self):
+    def train_and_test(self, display_test=True):
+        features = self.data_set.drop(['entropy', 'is_forged'], 1)
+        result = self.data_set['is_forged']
+        train_x, test_x, train_y, test_y = train_test_split(features, result, train_size=self.train_split, random_state=42)
         '''
-        all_features = self.data_set.drop(['is_forged'], axis=1)
-        variance_feat = self.data_set['variance'].values
-        curtosis_feat = self.data_set['curtosis'].values
-        skewness_feat = self.data_set['skewness'].values
-        entropy_feat = self.data_set['entropy'].values
-        is_forged = self.data_set['is_forged'].values
+        print(train_x.describe())
+        print(train_y.describe())
+
+        print(test_x.describe())
+        print(test_y.describe())
         '''
-        
-        train_data = self.data_set.sample(frac=0.8)
-        test_data = self.data_set.drop(train_data.index).reset_index(drop=True)
-        train_data = train_data.reset_index(drop=True)
+        self.svm_model = svm.SVC(gamma='auto',kernel='linear').fit(train_x, train_y)
 
-        train_X = train_data.drop(['entropy', 'is_forged'], 1)
-        train_Y = train_data['is_forged']
+        if (display_test):
+            z = lambda x,y: (-self.svm_model.intercept_[0]-self.svm_model.coef_[0][0]*x-self.svm_model.coef_[0][1]*y) / self.svm_model.coef_[0][2]
 
-        test_X = test_data.drop(['entropy', 'is_forged'], 1)
-        test_Y = test_data['is_forged']
+            tmp = [round(min(test_x.min(axis=1))),round(max(test_x.max(axis=1)))]
+            x,y = np.meshgrid(tmp,tmp)
 
-        '''
-        print(train_X.describe())
-        print(train_Y.describe())
+            pred_y = self.svm_model.predict(test_x)
+            acc = metrics.accuracy_score(test_y, pred_y)
 
-        print(test_X.describe())
-        print(test_Y.describe())
-        '''
+            # Plot stuff.
+            fig = graph.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot_surface(x, y, z(x,y), alpha=0.5)
+            scatter = ax.scatter(test_x['variance'], test_x['curtosis'], test_x['skewness'], c=test_y)
+            ax.set_xlabel('variance')
+            ax.set_ylabel('curtosis')
+            ax.set_zlabel('skewness')
+            graph.title('Classification plot using variance, curtosis and skewness (test data)\nAccuracy: {:.2%}'.format(acc))
+            graph.show()
 
-        SVM_Model = svm.SVC(gamma='auto',kernel="linear").fit(train_X, train_Y)
-
-        z = lambda x,y: (-SVM_Model.intercept_[0]-SVM_Model.coef_[0][0]*x-SVM_Model.coef_[0][1]*y) / SVM_Model.coef_[0][2]
-
-        tmp = np.linspace(round(min(test_X.min(axis=1))),2,round(max(test_X.max(axis=1))))
-        x,y = np.meshgrid(tmp,tmp)
-
-        # Plot stuff.
-        fig = graph.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(x, y, z(x,y))
-        scatter = ax.scatter(test_X['variance'], test_X['curtosis'], test_X['skewness'], c=test_Y)
-        ax.set_xlabel('variance')
-        ax.set_ylabel('curtosis')
-        ax.set_zlabel('skewness')
-        #ax.plot3D(X[Y==0,0], X[Y==0,1], X[Y==0,2],'ob')
-        #ax.plot3D(X[Y==1,0], X[Y==1,1], X[Y==1,2],'sr')
-        graph.show()
-
-
-
-
+    def predict(self, test_data):
+        try:
+            return self.svm_model.predict(test_data)
+        except:
+            return -1
